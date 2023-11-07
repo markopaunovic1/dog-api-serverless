41b543875fccf88e17eae60c274947acd509a3be
markopaunovic1 <marpau970514@hotmail.com>
2023-11-07 13:09:38 +0100

    first commit

---
+++ functions/getDogs/index.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+const { sendResponse } = require('../../responses');
+const db = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+
+    const {Items} = await db.scan({
+        TableName: 'dogs-db'
+    }).promise();
+
+    return sendResponse(200, {success: true, dogs : Items});
+
+}
