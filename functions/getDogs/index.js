5368f974a9130d4fc1e2d05fe2ea20cce8b8be35
markopaunovic1 <marpau970514@hotmail.com>
2023-11-20 20:32:20 +0100

    create and login function

--- functions/getDogs/index.js
+++ functions/getDogs/index.js
@@ -5,7 +5,12 @@ const db = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event, context) => {
 
     const {Items} = await db.scan({
-        TableName: 'dogs-db'
+        TableName: 'dogs-db',
+        FilterExpression: "attribute_exists(#id)",
+        ExpressionAttributeNames: {
+            "#id" : "id"
+        }
+
     }).promise();
 
     return sendResponse(200, {success: true, dogs : Items});
